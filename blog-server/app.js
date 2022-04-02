561f74fbb0cbe3a8265a4b9bb0ea544ee991f354
doublewhitee <doublewhite233@gmail.com>
2022-04-02 22:54:53 +0100

    init

---
+++ blog-server/app.js
@@ -0,0 +1,32 @@
+import express from 'express'
+import router from './routes/index.js'
+import config from 'config'
+import logger from 'morgan'
+import cookieParser from 'cookie-parser'
+import './mongoDB/db.js'
+
+const app = express()
+
+// 跨域
+app.all('*', (req, res, next) => {
+  // 设置请求头为允许跨域
+  res.header('Access-Control-Allow-Origin', req.headers.origin || '*');
+  // 设置服务器支持的所有头信息字段
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild, sessionToken');
+  // 设置服务器支持的所有跨域请求的方法
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Content-Type', 'application/json;charset=utf-8');
+  next();
+})
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+router(app)
+
+app.server = app.listen(config.port, () => {
+  console.log(`server running @ http://${ config.host }:${ config.port }`)
+})
